77c8aa4682ea07508a2cf4ae6ac20c3c05491bdd
Vincent Bosc <vincent.bosc@elastic.co>
2025-11-10 12:20:42 +0100

    baseurl change

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://vincentbosc.github.io/elasticsearch-search-consultant",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
